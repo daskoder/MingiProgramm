83bfdbbc5a093c1b6142ca9ecd4caa2422534b55
Opilane TTHK <tomas.jastremski@gmail.com>
2024-04-26 12:16:46 +0300

    Update Program.cs

--- Mingiprogramm/Program.cs
+++ Mingiprogramm/Program.cs
@@ -6,7 +6,36 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Looney tunes grade BS");
+<<<<<<< Updated upstream
             Console.WriteLine("Sen esimene featua");
+=======
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+            Console.WriteLine("Kennertil on raige skill issue");
+>>>>>>> Stashed changes
             
             
         
